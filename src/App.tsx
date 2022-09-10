ab9cbbfd985f050ac6f8d999ab4d776397e07970
Kamil <kkedzierski@apptension.com>
2022-09-10 13:38:17 +0200

    Added AsyncStorage session storage

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>TELOOO</p>
+        <p>TELOOasdasdO</p>
       </header>
     </div>
   );
